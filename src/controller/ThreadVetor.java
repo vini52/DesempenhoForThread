78d80454d44f1e48a7643e6582c976518a98753f
Vinicius Araujo <varaujo.silva03@gmail.com>
2024-03-08 21:26:40 -0300

    apply methods

--- src/controller/ThreadVetor.java
+++ src/controller/ThreadVetor.java
@@ -1,9 +1,38 @@
 package controller;
 
-public class ThreadVetor {
+import java.text.DecimalFormat;
 
-	public ThreadVetor() {
-		// TODO Auto-generated constructor stub
+public class ThreadVetor extends Thread{
+	int num;
+	int[] vetor;
+
+	public ThreadVetor(int num, int[] vetor) {
+		this.num = num;
+		this.vetor = vetor;
+	}
+	@Override
+	public void run() {
+		calcDesemp();
+	}
+	private void calcDesemp() {
+		if(num % 2 == 0) {
+			int inicio = (int) System.nanoTime();
+			for(int i = 0; i < vetor.length; i++) {
+				vetor[i] = vetor[i];
+			}
+			int fim = (int) System.nanoTime();
+			int tempo = (fim - inicio);
+			System.out.println("Tempo com for tradicional => " + tempo + " nanosegundos");
+		} else {
+			int inicio = (int) System.nanoTime();
+			for(int n:vetor) {
+				n = n;
+			}
+			int fim = (int) System.nanoTime();
+			int tempo = (fim - inicio);
+			System.out.println("Tempo com for each => " + tempo + " nanosegundos");
+		}
+		
 	}
 
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,16 @@
 package view;
+import controller.ThreadVetor;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] vetor = new int[1000];
+		for(int i = 0; i < 1000; i++)
+			vetor[i] = 0;
+		for(int i = 0; i < 2; i++) {
+			Thread t = new ThreadVetor(i, vetor);
+			t.start();
+		}
 
 	}
 
